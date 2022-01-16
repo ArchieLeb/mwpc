0f90cb85c41fb18fe9018b5b46d5c3626b4f7f7c
b2cmu <wolfgang.1990@yahoo.de>
2022-01-16 11:42:32 +0100

    Erste Anpassung

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -7,12 +7,12 @@ public class Main  {
         Model begruessung = new Model();
         System.out.println(begruessung.gethi());
 
-        JFrame hibox = new JFrame("Willkommen, willkommen");
+        JFrame hibox = new JFrame("Willkommen Michael");
         hibox.setSize(350,350);
         hibox.setLocationRelativeTo(null);
         hibox.add(new Panel(), BorderLayout.CENTER);
         hibox.setVisible(true);
-
+git 
     }
 }
 
